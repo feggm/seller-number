9e665af47cd6402d56c19b060a70a6c71e2ba6da
Kai Wittmann <kai.wittmann@feggm.de>
2025-09-06 15:03:15 +0200

    chore: introduce isLoading state for PageButton

--- src/components/PageButton.tsx
+++ src/components/PageButton.tsx
@@ -6,11 +6,13 @@ import { Button } from './ui/button'
 
 type PageButtonProps = Parameters<typeof Button>[0] & {
   counter?: ReactNode
+  isLoading?: boolean
 }
 
 export const PageButton = ({
   children,
   counter,
+  isLoading = false,
   className,
   ...props
 }: PageButtonProps) => {
@@ -18,6 +20,7 @@ export const PageButton = ({
     <motion.div whileHover={{ scale: 1.02 }} whileTap={{ scale: 0.98 }}>
       <Button
         variant="outline"
+        disabled={isLoading || props.disabled}
         className={cn(
           'relative bg-white hover:bg-slate-50 text-slate-800 border border-slate-200 shadow-sm px-5 py-6 h-auto font-medium uppercase rounded-lg ',
           className
@@ -25,6 +28,9 @@ export const PageButton = ({
         {...props}
       >
         {children}
+        {isLoading && (
+          <div className="absolute inset-0 bg-gray-200 animate-pulse rounded opacity-70" />
+        )}
         {counter !== undefined && (
           <span className="absolute -top-2 -right-2 bg-rose-500 text-white text-xs font-medium rounded-full w-6 h-6 flex items-center justify-center shadow-sm">
             {counter}
