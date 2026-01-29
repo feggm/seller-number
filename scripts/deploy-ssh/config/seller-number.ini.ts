0aefb516bc8b4c6aa916e56706f15ce13439e5ea
Kai Wittmann <kai.wittmann@feggm.de>
2026-01-29 14:50:35 +0100

    chore(deploy-ssh): add explicit logging config

--- scripts/deploy-ssh/config/seller-number.ini.ts
+++ scripts/deploy-ssh/config/seller-number.ini.ts
@@ -2,6 +2,9 @@ export const sellerNumberConfig = () =>
   `[program:seller-number]
 directory=%(ENV_HOME)s/projects/seller-number
 command=%(ENV_HOME)s/projects/seller-number/pocketbase.bin serve --http :8090
+stdout_logfile=%(ENV_HOME)s/logs/seller-number.log
+stderr_logfile=%(ENV_HOME)s/logs/seller-number.err.log
+redirect_stderr=false
 autostart=yes
 autorestart=yes
 startsecs=60
